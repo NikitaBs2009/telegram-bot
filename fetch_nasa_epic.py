f98dd2acdc694170c27bf7430744084236e07c8b
NikitaBs2009 <102821772+NikitaBs2009@users.noreply.github.com>
2023-01-12 19:50:33 +0300

    удалил не использованный код

--- fetch_nasa_epic.py
+++ fetch_nasa_epic.py
@@ -16,13 +16,10 @@ def fetch_nasa_epic(api_key):
     response = requests.get(url, params=payload)
     response.raise_for_status()
     for image in response.json():
-        pprint(image["image"])
-        pprint(image["date"])
         datetime_date = datetime.strptime(image["date"] , "%Y-%m-%d %H:%M:%S")
         format_date = (datetime_date.strftime('%Y/%m/%d'))
         image_path = os.path.join('images',f"{image['image']}).png")
         image_link = f"https://api.nasa.gov/EPIC/archive/natural/{format_date}/png/{image['image']}.png"
-        print (image_link)
         download(image_link, image_path, params=payload)
 if __name__ == '__main__':
     load_dotenv()
