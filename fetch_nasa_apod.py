ecb8dbeb9bb68c76bb88c2ac319868080bfd6641
NikitaBs2009 <102821772+NikitaBs2009@users.noreply.github.com>
2023-01-27 19:38:31 +0300

    Исправил ошибку

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -8,9 +8,9 @@ import requests
 
 from tools import get_extension
 
-def fetch_nasa_apod(nasa_api_key,count):
+def fetch_nasa_apod(nasa_api_key, count):
     url = 'https://api.nasa.gov/planetary/apod'
-    payload = {'nasa_api_key': nasa_api_key,'count':count}
+    payload = {'api_key': nasa_api_key,'count':count}
     response = requests.get(url, params=payload)
     response.raise_for_status()
 
@@ -26,4 +26,4 @@ if __name__ == '__main__':
     nasa_api_key = os.environ["NASA_API_KEY"]
     photo_quantity = os.getenv("FOTO_QUANTITY",30)
     os.makedirs('images', exist_ok=True)
-    fetch_nasa_apod(nasa_api_key)
+    fetch_nasa_apod(nasa_api_key,photo_quantity)
