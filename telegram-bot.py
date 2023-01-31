76dc692eebf5088bcf773fbcd1147299786c73b0
NikitaBs2009 <102821772+NikitaBs2009@users.noreply.github.com>
2023-01-31 18:43:02 +0300

    Правки
     исправил на os.environ

--- telegram-bot.py
+++ telegram-bot.py
@@ -10,8 +10,9 @@ from dotenv import load_dotenv
 
 if __name__ == '__main__':
     load_dotenv()
-    tg_token = os.getenv("TG_TOKEN")
-    chat_id = os.getenv("TG_CHAT_ID")
+    os.environ['TG_TOKEN']
+    tg_token = os.environ['TG_TOKEN']
+    chat_id = os.environ['TG_CHAT_ID']
     bot = telegram.Bot(token=tg_token)
     filesindir = os.listdir("images")
 
